b249d7d695fd363f2776d69985fd209ec76a353a
Eugene-im <em.92@mail.ru>
2018-10-30 20:35:45 +0200

    asd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-    $('.slider,.slide_1,.slide_2,.slide_3').slick({
-      
-    });
+$(document).ready(function() {
+  $(".slider").slick({});
+  $(".slide_1,.slide_2,.slide_3").slick({
+    slidesToShow: 3,
+    slidesToScroll: 2
   });
+
+  $('#tab1').click(function(){
+    $('slide_1').toggleClass('active')
+  });
+  $('#tab2').click(function(){
+    $('slide_2').toggleClass('active')
+  });
+  $('#tab3').click(function(){
+    $('slide_3').toggleClass('active')
+  });
+});
