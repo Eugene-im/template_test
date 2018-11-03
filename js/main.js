084336ff8f633fbe6ef41f9b35ff205ce90911ab
Eugene-im <em.92@mail.ru>
2018-11-03 15:02:12 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
-  $(".slider").slick({});
+  $(".slider").slick({
+    arrows:false
+  });
   $(".slide_1,.slide_2,.slide_3").slick({
-    slidesToShow: 3,
-    slidesToScroll: 2
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    infinite:true,
+    dots:false,
+    arrows:true
   });
 
   $('#tab1').click(function(){
-    $('slide_1').toggleClass('active')
+    $('#tab1 span').addClass('act');
+    $('#tab2 span').removeClass('act');
+    $('#tab3 span').removeClass('act');
+    $('.slide_1').addClass('active');
+    $('.slide_2').removeClass('active');
+    $('.slide_3').removeClass('active');
   });
   $('#tab2').click(function(){
-    $('slide_2').toggleClass('active')
+    $('#tab1 span').removeClass('act');
+    $('#tab2 span').addClass('act');
+    $('#tab3 span').removeClass('act');
+    $('.slide_1').removeClass('active');
+    $('.slide_2').addClass('active');
+    $('.slide_3').removeClass('active');
   });
   $('#tab3').click(function(){
-    $('slide_3').toggleClass('active')
+    $('#tab1 span').removeClass('act');
+    $('#tab2 span').removeClass('act');
+    $('#tab3 span').addClass('act');
+    $('.slide_1').removeClass('active');
+    $('.slide_2').removeClass('active');
+    $('.slide_3').addClass('active');
   });
 });
